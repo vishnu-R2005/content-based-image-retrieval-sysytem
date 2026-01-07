c43279d63b4c949f9a90dc0395e5c230462b8906
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-01-07 23:14:54 +0530

    Update urls.py

--- cbir_backend/users/urls.py
+++ cbir_backend/users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import RegisterView, login_view, user_view, promote_user_view
+from .views import (
+    RegisterView,
+    login_view,
+    user_view,
+    promote_user_view,
+)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', login_view, name='login'),
-    path('user/', user_view, name='user'),
-    path('promote/<int:user_id>/', promote_user_view, name='promote-user'),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", login_view, name="login"),
+    path("user/", user_view, name="user"),
+    path("promote/<int:user_id>/", promote_user_view, name="promote"),
 ]
-

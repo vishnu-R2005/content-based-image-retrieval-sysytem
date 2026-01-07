06affa14f0db30700bb4e4baecb00ff3e5714558
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-01-07 22:39:35 +0530

    Update settings.py

--- cbir_backend/cbir_backend/settings.py
+++ cbir_backend/cbir_backend/settings.py
@@ -136,11 +136,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",  # 🔥 REQUIRED
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
 }
+
 
 # ==================================================
 # JWT
